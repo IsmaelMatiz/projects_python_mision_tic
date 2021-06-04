a1e462bace0fb8526c8e541efd783f61ba7eb978
Ismael Matiz <ismaelmatiz@gmail.com>
2021-06-04 06:56:49 -0500

    primera version de imc, siguiente paso, hacer que se ejecute desde una ventana

---
+++ imc.py
@@ -0,0 +1,16 @@
+print("dime tu estatura en metros y peso en kilos ")
+peso = 0
+altura = 0.00
+while peso <= 0 or peso >= 600:
+   peso = int(input("Dime tu peso en kilos: "))
+while altura <= 0 or altura >= 3:
+    altura = float(input("Dime tu estatura en metro: "))
+imc = round(peso / (altura ** 2), 1)
+if imc < 18.5:
+    print("Delgadez o bajo peso o desnutrido")
+elif imc >= 18.5 and imc <= 24.9:
+    print("Normal")
+elif imc > 24.9 and imc <= 29.9:
+    print("Peso superior al normal")
+else:
+    print("Obesidad")
